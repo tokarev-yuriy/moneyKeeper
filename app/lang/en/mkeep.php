de8be518a8e210117d7c8372037a15c4afab9f0f
Yuriy Tokarev <yuriytok@gmail.com>
2017-10-08 01:33:35 +0300

    Add localization

---
+++ app/lang/en/mkeep.php
@@ -0,0 +1,82 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| MoneyKeep Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain menu's, type's and other money keep labels.
+	|
+	*/
+
+    "title" => "Money",
+    "spends" => "Spends",
+    "incomes" => "Incomes",
+    "transfers" => "Transfers",
+    "wallets" => "Wallets",
+    "categories" => "Categories",
+    "logout" => "Logout",
+    "login" => "Login",
+    "email" => "E-Mail Address",
+    "password" => "Password",
+    "remember" => "Remember Me",
+    "register" => "Register",
+    "name" => "Name",
+    "confirm_password" => "Confirm Password",
+    "sort" => "Sort",
+    "start" => "Start",
+    "save" => "Save",
+    "type" => "Type",
+    "date" => "Date",
+    "summ" => "Summ",
+    "category" => "Category",
+    "comment" => "Comment",
+    "src_wallet" => "Source Wallet",
+    "dest_wallet" => "Destination Wallet",
+    "wallet" => "Wallet",
+    "wrong_pass" => "Wrong Password or Login",
+    
+    "items" => "Items",
+    "add_item" => "Add item",
+    "edit_item" => "Edit item",
+    "delete_item" => "Delete item",
+    
+    "categories" => "Categories",
+    "add_category" => "Add category",
+    "edit_category" => "Edit category",
+    "delete_category" => "Delete category",
+    
+    "wallets" => "Wallets",
+    "add_wallet" => "Add wallet",
+    "edit_wallet" => "Edit wallet",
+    "delete_wallet" => "Delete wallet",
+    
+    "operations" => "Operations",
+    "add_operation" => "Add operation",
+    "edit_operation" => "Edit operation",
+    "delete_operation" => "Delete operation",
+    
+    "spends" => "Spends",
+    "add_spend" => "Add spend",
+    "edit_spend" => "Edit spend",
+    "delete_spend" => "Delete spend",
+    
+    "incomes" => "Incomes",
+    "add_income" => "Add income",
+    "edit_income" => "Edit income",
+    "delete_income" => "Delete income",
+    
+    "transfers" => "Transfers",
+    "add_transfer" => "Add transfer",
+    "edit_transfer" => "Edit transfer",
+    "delete_transfer" => "Delete transfer",
+    
+    "spend" => "Spend",
+    "income" => "Income",
+    "transfer" => "Transfer",
+    "any" => "Any",
+
+
+);
