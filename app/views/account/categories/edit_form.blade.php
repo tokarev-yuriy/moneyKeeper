c71efe914ea0f83706178e9212ed2c55dd700ae3
Yuriy Tokarev <yuriytok@gmail.com>
2017-10-08 01:30:55 +0300

    CRUD for categories

---
+++ app/views/account/categories/edit_form.blade.php
@@ -0,0 +1,50 @@
+   <?=Form::open(array('url' => ((isset($obItem) && $obItem->id)?$paths['update'].'/'.$obItem->id:$paths['add'])))?>
+
+        <div class="row form-group">
+            <label for="name" class="col-md-4 control-label"><?=trans('mkeep.name')?></label>
+            <div class="col-md-6">
+                <?=Form::text('name', Input::get('name', isset($obItem)?$obItem->name:''), array('class'=>(isset($errors) && $errors->has('name') ? 'form-control is-invalid' : 'form-control')))?>
+
+                <span class="invalid-feedback">
+                @if (isset($errors) && $errors->has('name'))
+                        <strong>{{ $errors->first('name') }}</strong>
+                @endif
+                </span>
+            </div>
+        </div>
+        
+        <div class="row form-group">
+            <label for="sort" class="col-md-4 control-label"><?=trans('mkeep.sort')?></label>
+            <div class="col-md-6">
+                <?=Form::text('sort', Input::get('sort', isset($obItem)?$obItem->sort:''), array('class'=>(isset($errors) && $errors->has('sort') ? 'form-control is-invalid' : 'form-control')))?>
+
+                <span class="invalid-feedback">
+                @if (isset($errors) && $errors->has('sort'))
+                        <strong>{{ $errors->first('sort') }}</strong>
+                @endif
+                </span>
+            </div>
+        </div>
+        
+        <div class="row form-group">
+            <label for="type" class="col-md-4 control-label"><?=trans('mkeep.type')?></label>
+            <div class="col-md-6">
+                <?=Form::select('type', Category::getTypeList(), Input::get('type', isset($obItem)?$obItem->type:''), array('class'=>(isset($errors) && $errors->has('type') ? 'form-control is-invalid' : 'form-control')))?>
+
+                <span class="invalid-feedback">
+                @if (isset($errors) && $errors->has('type'))
+                        <strong>{{ $errors->first('type') }}</strong>
+                @endif
+                </span>
+            </div>
+        </div>
+
+        <div class="row form-group">
+            <div class="col-md-4"></div>
+            <div class="col-md-6">
+                <button type="submit" class="btn btn-success">
+                    <i class="fa fa-btn fa-save"></i> <?=trans('mkeep.save')?>
+                </button>
+            </div>
+        </div>
+    <?=Form::close();?>
