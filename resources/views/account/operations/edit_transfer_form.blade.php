c4eb5048d6aa4972dd22437ed7f6b3d31e97fec8
Yuriy Tokarev <yuriytok@gmail.com>
2018-07-04 14:18:11 -0400

    Transfer operation fix

--- resources/views/account/operations/edit_transfer_form.blade.php
+++ resources/views/account/operations/edit_transfer_form.blade.php
@@ -56,7 +56,7 @@
      <div class="row form-group">
          <label for="wallet_from_id" class="col-md-4 control-label">{{ trans('mkeep.src_wallet') }}</label>
          <div class="col-md-6">
-             {!! Form::select('wallet_to_id', \App\MoneyKeeper\Models\Operation::getWallets(), Input::get('wallet_to_id', isset($obItem)?$obItem->wallet_to_id:Session::get('wallet_to_id')), array('class'=>(isset($errors) && $errors->has('wallet_to_id') ? 'form-control is-invalid' : 'form-control'))) !!}
+             {!! Form::select('wallet_from_id', \App\MoneyKeeper\Models\Operation::getWallets(), Input::get('wallet_from_id', isset($obItem)?$obItem->wallet_from_id:Session::get('wallet_from_id')), array('class'=>(isset($errors) && $errors->has('wallet_from_id') ? 'form-control is-invalid' : 'form-control'))) !!}
 
              <span class="invalid-feedback">
              @if (isset($errors) && $errors->has('wallet_from_id'))
