93e5ffeb7a5f1f50375b812650d5b546cfa444b7
Yuriy Tokarev <yuriytok@gmail.com>
2022-10-10 02:21:48 +0400

    Add getData macros for a collection of entities

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Collection::macro('getData', function () {
+            return $this->map(function ($value) {
+                return $value->toArray();
+            });
+        });
     }
 
     /**
